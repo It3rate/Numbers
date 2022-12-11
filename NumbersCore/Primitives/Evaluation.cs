709b17d84310fd9218dd0bd2879b0295903f4291
Robin Debreuil <debreuil.robin@gmail.com>
2022-12-11 09:19:43 -0600

    First structure of Eval Flags.

---
+++ NumbersCore/Primitives/Evaluation.cs
@@ -0,0 +1,55 @@
+﻿namespace NumbersCore.Primitives
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    [Flags]
+    public enum EvalFlag
+    {
+        Trait,
+        Domain,
+        BasisFocal,
+        BasisOrigin,
+        BasisResolution,
+        MinMax,
+        Length,
+        HasOverlap,
+        Equal,
+        Contains,
+        ContainedBy,
+        WhollyGreater,
+        WhollyLess,
+        PartiallyGreater,
+        PartiallyLess,
+        Wrapped,
+        Bounced,
+        Overflowed,
+    }
+    public class Evaluation
+    {
+        // Flags:
+        // same trait, domain, basis, resolution, minmax
+        // direction (same as basis, same abs)
+        // length
+        // overlap
+        // per endpoint - gt, lt, eq, per basis/abs
+        // containment
+        // same mod, whole
+        // has wrapped/bounced/overflowed
+
+        public Number Source { get; }
+        public Number Target { get; }
+        public EvalFlag TestFlags { get; } = (EvalFlag) 0x7FFFFFFF;
+        public EvalFlag ResultFlags { get; } = 0;
+        public bool Result { get; }
+
+        public bool Update()
+        {
+            // compare Numbers for the testFlag matches. (maybe just test all, then & with TestFlags)
+	        return Result;
+        }
+    }
+}
